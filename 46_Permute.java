468ccf06c1e61044dc9e9da1baf6f9b7ce3ca239
CD <1595292846@qq.com>
2025-09-24 11:07:11 +0800

    250924:solve 46

---
+++ 46_Permute.java
@@ -0,0 +1,25 @@
+public class 46_Permute {
+
+    public List<List<Integer>> permute(int[] nums) {
+        if (nums.length == 0) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+
+        backtrack(result, new ArrayList<>(), nums);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums) {
+        if (tempList.size() == nums.length) {
+            result.add(new ArrayList<>(tempList));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (tempList.contains(nums[i])) continue; // skip duplicates
+            tempList.add(nums[i]);
+            backtrack(result, tempList, nums);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
