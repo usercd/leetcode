4f94eadfd14f0bdf9498c2263d9a013186b4d29b
CD <75894942+usercd@users.noreply.github.com>
2025-10-19 19:35:13 +0800

    251019:solve problem 55 & 121

---
+++ DynamicProgramming/Code_121_BestTimeToBuyAndSellStock.java
@@ -0,0 +1,19 @@
+package DynamicProgramming;
+
+public class Code_121_BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            }
+        }
+        return maxProfit;
+    }
+}
---
+++ DynamicProgramming/Code_55_JumpGame.java
@@ -0,0 +1,20 @@
+package DynamicProgramming;
+
+public class Code_55_JumpGame {
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
