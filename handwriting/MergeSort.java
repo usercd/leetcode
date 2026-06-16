e3760dfdb6b57efd26858f58c93935447292d4d2
CD <cd@me.com>
2026-06-16 17:07:57 +0800

    260616

--- handwriting/MergeSort.java
+++ handwriting/MergeSort.java
@@ -37,4 +37,44 @@ public class MergeSort {
 
         System.arraycopy(temp, 0, arr, left, temp.length);
     }
+
+    public static void main(String[] args) {
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+        mergeSort(arr, 0, arr.length - 1);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void mergeSort(int[] arr) {
+        if (arr == null || arr.length < 2) return;
+        int[] aux = new int[arr.length];
+        sort(arr, aux, 0, arr.length - 1);
+    }
+
+    private static void sort(int[] arr, int[] aux, int left, int right) {
+        int mid = left + (right - left) / 2;
+        sort(arr, aux, left, mid);
+        sort(arr, aux, mid + 1, right);
+        if (arr[mid] <= arr[mid + 1]) return;
+        merge(arr, aux, left, mid, right);
+    }
+
+    private static void merge(int[] arr, int[] aux, int left, int mid, int right) {
+        // 只拷贝需要合并的区间到辅助数组
+        System.arraycopy(arr, left, aux, left, right - left + 1);
+
+        int i = left, j = mid + 1;
+        for (int k = left; k <= right; k++) {
+            if (i > mid) {
+                arr[k] = aux[j++];
+            } else if (j > right) {
+                arr[k] = aux[i++];
+            } else if (aux[j] < aux[i]) {
+                arr[k] = aux[j++];
+            } else {
+                arr[k] = aux[i++];
+            }
+        }
+    }
 }
